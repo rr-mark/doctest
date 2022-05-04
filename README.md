70559d7d0149a4dd0d239d9c714c1381f0d53a44
Mark Johnson <mark@rivelinrobotics.com>
2022-05-04 12:04:06 +0100

    Cleaned branch.

--- README.md
+++
@@ -1,4 +0,0 @@
-# doctest
-Test of documentation generation on push
-
-See [docs](https://github.com/rr-mark/doctest/tree/gh-pages/internal/doc/html)
--- internal/src/under_score.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * @file
- * @brief The name of this file contains an underscore.
- */
- 
- /// The name of this class contains an underscore too.
-class : under_score{
-}
