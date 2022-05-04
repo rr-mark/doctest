cb892f32a224779a59b53a1a7c72c95ddd3083e9
Mark Johnson <mark@rivelinrobotics.com>
2022-05-04 11:59:05 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# doctest
+Test of documentation generation on push
+
+See [docs](https://github.com/rr-mark/doctest/tree/gh-pages/internal/doc/html)
---
+++ internal/src/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * @brief This is a file.
+ */
+#include<iostream>
+
+/// Prints `"Hello, World!"`.
+int main(void){
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
